e4506ac6cd9f1723b084c3a9021f697e931eaa16
Bobby Brockway <bobby.brockway@gmail.com>
2025-04-20 00:10:02 +0100

    add home-manager

--- flake.nix
+++ flake.nix
@@ -4,13 +4,23 @@
   inputs = {
     # NixOS official package source, using the nixos-24.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.bbrockway = import ./home.nix;
+        }
       ];
     };
   };
